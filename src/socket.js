1b61e95aa0a2ce6dd0a3973e4cb9d4567ec0cb25
Brendan O'Keefe <c-brendan.okeefe@charter.com>
2019-10-14 12:53:00 -0600

    fix

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import openSocket from 'socket.io-client';
-const socket = openSocket('localhost:8080');
+const socket = openSocket('http://localhost:8080');
 
 function subscribeToRooms(cb) {
   socket.on('rooms', rooms => {
