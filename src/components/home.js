d067409aaf251a9001be9a86285822b7074c9a34
Brendan O'Keefe <c-brendan.okeefe@charter.com>
2019-10-16 14:35:10 -0600

    trying more shit

--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './home.css';
 
 import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:8080');
+const socket = openSocket('ws://127.0.0.1:8080');
 
 export class Home extends React.Component {
   state = {
