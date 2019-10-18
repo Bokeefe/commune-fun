f4634ca64de81a7beffedfb8068653243fd61938
Brendan O'Keefe <c-brendan.okeefe@charter.com>
2019-10-18 09:59:24 -0600

    heoku spec

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import Room from './components/room';
 class App extends React.Component {
   state = {
     callSign: '',
-    endpoint: 'ws://127.0.0.1:8080',
+    endpoint: 'ws://commune-games.herokuapp.com:8080',
     response: false,
     socket: null
   };
