ccd7d99ed229ac71a54ae99ead4c32e1636c1d73
Brendan O'Keefe <c-brendan.okeefe@charter.com>
2019-10-18 10:24:27 -0600

    ssl port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 443;
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
